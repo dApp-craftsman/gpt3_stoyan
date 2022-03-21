873be58cbb129fd81856287f6ef230218bd839f2
Stopalov12 <stopalov111@gmail.com>
2022-03-21 02:42:40 +0200

    GPT-3 Finished

---
+++ src/components/cta/CTA.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./cta.css";
+
+const cta = () => {
+  return (<div className="gpt3__cta">
+    <div className="gpt3__cta-content">
+      <p>Request Early Access to Get Started</p>
+      <h3>Register today & start exploring the endless possiblities.</h3>
+    </div>
+    <div className="gpt3__cta-button">
+      <button type="button">Get Started</button>
+    </div>
+  </div>)
+};
+
+export default cta;
